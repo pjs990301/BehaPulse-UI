6040b1bd10af097260b4d8998530a86de5842ed9
Pio <p990301@gmail.com>
2024-08-28 07:51:35 +0900

    :construction: Img 추가 및 불필요 의존성 삭제

--- database/db_connector.py
+++ database/db_connector.py
@@ -1,13 +1,5 @@
 import json
-import mysql.connector
 
 with open('config/db_config.json', 'r') as f:
     db_config = json.load(f)
 
-# db = mysql.connector.connect(
-#     host=db_config['Database']['host'],
-#     user=db_config['Database']['user'],
-#     password=db_config['Database']['password'],
-#     database=db_config['Database']['database'],
-#     auth_plugin='mysql_native_password'
-# )
